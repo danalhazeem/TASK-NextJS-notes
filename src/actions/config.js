2ddf97fee402abdcfed99fed0215b86ccc8903c8
danalhazeem <92695924+danalhazeem@users.noreply.github.com>
2024-10-24 12:17:27 +0300

    notes

--- src/actions/config.js
+++ src/actions/config.js
@@ -1,12 +1,12 @@
 const baseUrl = "https://task-react-auth-backend.eapi.joincoded.com/api";
 
 async function getHeaders() {
-  const headers = new Headers()
-  headers.append("Content-Type", "application/json")
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
   // Add an authorization header
+ 
 
-  return headers
+  return headers;
 }
 
-
-export { getHeaders, baseUrl }
+export { getHeaders, baseUrl };
